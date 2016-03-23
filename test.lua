631f64465ff276dd774623278483bf9c07b9c6f1
Sean Conner <spc@conman.org>
2016-03-23 01:45:09 -0400

    Update test---it was wrong in a few places.

--- test.lua
+++ test.lua
@@ -21,25 +21,25 @@
 
 local ddt  = require "org.conman.debug"
 local math = require "math"
-local cbor = require "cbor"
+local cbor = require "cbore"
 
 assert(cbor.encode(false)       == '\244')
 assert(cbor.encode(true)        == '\245')
 assert(cbor.encode(nil)         == "\246")
 assert(cbor.encode(math.huge)   == '\249\124\0')
 assert(cbor.encode(.5)          == '\249\56\0')
-assert(cbor.encode(1.0/65536.0) == '\250\55\192\0\0')
+assert(cbor.encode(6.09755516052246093750e-05) == '\250\56\127\224\0')
 assert(cbor.encode(math.pi)     == '\251\64\9\33\251\84\68\45\24')
 
 assert(cbor.encode(0)           == '\0')
 assert(cbor.encode(100)         == '\24\100')
-assert(cbor.encode(1000)        == '\25\0\232')
+assert(cbor.encode(1000)        == '\25\03\232')
 assert(cbor.encode(100000)      == '\26\0\1\134\160')
 assert(cbor.encode(8589934592)  == '\27\0\0\0\2\0\0\0\0')
 
 assert(cbor.encode(-1)          == '\32')
 assert(cbor.encode(-100)        == '\56\99')
-assert(cbor.encode(-1000)       == '\57\0\231')
+assert(cbor.encode(-1000)       == '\57\03\231')
 assert(cbor.encode(-100000)     == '\58\0\1\134\159')
 assert(cbor.encode(-8589934592) == '\59\0\0\0\1\255\255\255\255')
 
@@ -48,5 +48,5 @@ assert(cbor.encode("\225\254\162\225\154\177") == '\70\225\254\162\225\154\177')
 
 assert(cbor.encode("test")                     == "dtest")
 assert(cbor.encode("\225\154\162\225\154\177") == '\102\225\154\162\225\154\177')
-
-ddt.hexdump(cbor.encode {})
+assert(cbor.encode({false,true,0,1})           == '\132\244\245\0\1')
+assert(cbor.encode({[false]=0,[true]=1,two=2}) == '\163\244\0\245\1\99two\2')
